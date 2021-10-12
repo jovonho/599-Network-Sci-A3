b401102422483ac4216eed755c3195d0c2c0bb1e
Loic Ho-Von <loic.hovon@gmail.com>
2021-10-12 10:51:53 -0400

    q1

---
+++ gcn.py
@@ -0,0 +1,128 @@
+import numpy as np
+import pickle as pkl
+import networkx as nx
+import scipy.sparse as sp
+import sys
+
+
+def parse_index_file(filename):
+    """Parse index file."""
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+def sample_mask(idx, l):
+    """Create mask."""
+    mask = np.zeros(l)
+    mask[idx] = 1
+    return np.array(mask, dtype=bool)
+
+"""
+    Adapted from the original
+    https://github.com/tkipf/gcn/blob/39a4089fe72ad9f055ed6fdb9746abdcfebc4d81/gcn/utils.py
+"""
+def load_data(dataset_str, add_val_to_test=False):
+    """
+    Loads input data from gcn/data directory
+    ind.dataset_str.x => the feature vectors of the training instances as scipy.sparse.csr.csr_matrix object;
+    ind.dataset_str.tx => the feature vectors of the test instances as scipy.sparse.csr.csr_matrix object;
+    ind.dataset_str.allx => the feature vectors of both labeled and unlabeled training instances
+        (a superset of ind.dataset_str.x) as scipy.sparse.csr.csr_matrix object;
+    ind.dataset_str.y => the one-hot labels of the labeled training instances as numpy.ndarray object;
+    ind.dataset_str.ty => the one-hot labels of the test instances as numpy.ndarray object;
+    ind.dataset_str.ally => the labels for instances in ind.dataset_str.allx as numpy.ndarray object;
+    ind.dataset_str.graph => a dict in the format {index: [index_of_neighbor_nodes]} as collections.defaultdict
+        object;
+    ind.dataset_str.test.index => the indices of test instances in graph, for the inductive setting as list object.
+    All objects above must be saved using python pickle module.
+    :param dataset_str: Dataset name
+    :return: All data input files loaded (as well the training/test data).
+    """
+    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+    objects = []
+    for i in range(len(names)):
+        with open("data/gcn/ind.{}.{}".format(dataset_str, names[i]), 'rb') as f:
+            if sys.version_info > (3, 0):
+                objects.append(pkl.load(f, encoding='latin1'))
+            else:
+                objects.append(pkl.load(f))
+
+    x, y, tx, ty, allx, ally, graph = tuple(objects)
+    test_idx_reorder = parse_index_file("data/gcn/ind.{}.test.index".format(dataset_str))
+    test_idx_range = np.sort(test_idx_reorder)
+
+    print(len(test_idx_range))
+
+    print(f"x {x.shape}")
+    print(f"tx {tx.shape}")
+    print(f"allx {allx.shape}")
+
+    print(f"y {y.shape}")
+    print(f"ty {ty.shape}")
+    print(f"ally {ally.shape}")
+
+    if dataset_str == 'citeseer':
+        # Fix citeseer dataset (there are some isolated nodes in the graph)
+        # Find isolated nodes, add them as zero-vecs into the right position
+        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
+        # tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+        # tx_extended[test_idx_range-min(test_idx_range), :] = tx
+        # tx = tx_extended
+        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+        ty_extended[test_idx_range-min(test_idx_range), :] = ty
+        ty = ty_extended
+
+    G = nx.from_dict_of_lists(graph)
+
+    labels = np.vstack((ally, ty))
+    labels[test_idx_reorder, :] = labels[test_idx_range, :]
+
+    # 3327 * 6
+    print(labels.shape)
+
+    idx_test = test_idx_range.tolist()
+    idx_train = range(len(y))
+    idx_val = range(len(y), len(y)+500)
+
+    # There are 15 nodes with no category in citeseer
+    # We will arbitrarily assign them to category 0 
+    # since its easier than modifying the graph object to remove them
+    if dataset_str == 'citeseer':
+        labels_all = []
+
+        for line in labels:
+            category = np.where(line == 1)
+            if len(category[0]) > 0:
+                category = int(category[0][0])
+            else:
+                category = 0
+            labels_all.append(category)
+        
+        labels = labels_all
+    else:
+        labels = list(labels.nonzero()[1])
+
+    # Add the train labels to the graph
+    for i in idx_train:
+        G.nodes[i]['label'] = labels[i]
+
+    # Optionally add the labels of the validation set to the graph as well
+    # Effectively adding the validation set to the test set
+    if add_val_to_test:
+        for i in idx_val:
+            G.nodes[i]['label'] = labels[i]
+
+    return G, labels, idx_test
+
+
+if __name__=='__main__':
+
+    G, labels, idx_test = load_data("citeseer")
+
+    print(G.nodes(data=True))
+
+    print(idx_test)
+
+    
+    
--- main.py
+++ main.py
@@ -1,4 +1,148 @@
-﻿
+﻿import json
+from os import replace
+import time
+from igraph import Graph
+import numpy as np
+from numpy.lib.shape_base import split
+import gcn
+from networkx.algorithms.node_classification import local_and_global_consistency, harmonic_function
+from networkx.classes.function import is_directed, to_undirected, to_directed
+from sklearn.metrics import normalized_mutual_info_score, adjusted_rand_score, accuracy_score
+import matplotlib.pyplot as plt
+import gcn
+
+def load_dataset(dataset):
+
+	real_classic = ["karate", "football", "polblogs", "polbooks", "strike"]
+	real_node_label = ["citeseer", "cora", "pubmed"]
+
+	print(f"\nDataset {dataset}")
+
+	if dataset in real_classic:
+		G = Graph.Read_GML(f"./data/real-classic/{dataset}.gml")
+		G = G.to_networkx()
+
+		# Polbooks dataset has string labels which throws a ValueError when we 
+		# try the first line. We convert it to int using ord().
+		if dataset == 'polbooks':
+			polbooks_map = {
+				'l': 1,
+				'c': 2,
+				'n': 3
+			}
+			for node in G.nodes:
+				prev = G.nodes[node]['value']
+				G.nodes[node]['value'] = polbooks_map[prev]
+
+		try:
+			labels_true = [ int(G.nodes[node]['value']) for node in G.nodes ]
+		except ValueError:
+			labels_true = [ ord(G.nodes[node]['value']) for node in G.nodes ]
+
+		n_communities = len(np.unique(labels_true))
+
+	# Requested a GCN graph
+	elif dataset in real_node_label:
+		G, labels_true, n_communities = gcn.load_data(dataset)
+
+	print(f"Real number of communities: {n_communities}")
+
+	if is_directed(G):
+		G = to_undirected(G)
+
+	return G, np.array(labels_true), n_communities
+
+
 
 if __name__=='__main__':
-	pass
+
+
+	rng = np.random.default_rng()
+	datasets = [ "polbooks", "karate", "football", "polblogs", "strike"]
+
+	for dataset in datasets:
+		G, labels_true, _ = load_dataset(dataset)
+
+		num_nodes = len(labels_true)
+		print(num_nodes)
+
+		NMIs = []
+		ARIs = []
+		Accs = []
+		for n in range(5, 100, 5):	
+
+			portion_drop =  n/100
+			test_size = round(num_nodes * portion_drop)
+
+			print(f"Dropping {n}% of labels. Test set size: {test_size}")
+
+			# Repeat 10 times for each test set size
+			nmi = []
+			ari = []
+			acc = []
+			for _ in range(10):
+				# Copy the graph bc we will be deleting attributes
+				G_ = G.copy()
+
+				# Generate random indices that will define the test set
+				test_idx = rng.choice(num_nodes, size=test_size, replace=False)
+				test_idx = np.sort(test_idx)
+
+				# Extract test set labels
+				y_test = np.array(labels_true)[test_idx]
+
+				# Delete labels of test set in the graph
+				for i in test_idx:
+					del G_.nodes[i]['value']
+
+				# Get test set predicted labels 
+				y_pred = np.array(local_and_global_consistency(G_, label_name='value'), dtype=int)
+				y_pred = y_pred[test_idx]
+
+				# Score the predicted labels using various measures
+				nmi.append(normalized_mutual_info_score(y_test, y_pred))
+				ari.append(adjusted_rand_score(y_test, y_pred))
+				acc.append(accuracy_score(y_test, y_pred))
+
+			NMI = np.mean(nmi)
+			ARI = np.mean(ari)
+			Acc = np.mean(acc)
+			print(f"NMI: {NMI}\nARI: {ARI}\nAcc: {Acc}\n")
+
+			NMIs.append(NMI)
+			ARIs.append(ARI)
+			Accs.append(Acc)
+
+
+		fig, ax = plt.subplots(figsize=(9,6))
+		x_axis = [x for x in range(5, 100, 5)]
+
+		ax.plot(x_axis, Accs, c='pink', label='Accuracy')
+		ax.plot(x_axis, ARIs, c='orange', label='ARI')
+		ax.plot(x_axis, NMIs, c='purple', label='NMI')
+
+		ax.set_xlabel("Test Set Size (%)")
+		ax.set_ylabel("Score")
+		ax.legend()
+
+		# Major ticks every 20, minor ticks every 5
+		x_major_ticks = np.arange(0, 101, 20)
+		x_minor_ticks = np.arange(0, 101, 5)
+		y_major_ticks = np.arange(0, 1, .2)
+		y_minor_ticks = np.arange(0, 1, .05)
+
+		ax.set_xticks(x_major_ticks)
+		ax.set_xticks(x_minor_ticks, minor=True)
+		ax.set_yticks(y_major_ticks)
+		ax.set_yticks(y_minor_ticks, minor=True)
+		
+		ax.grid(True, which='both', linestyle='--')
+		ax.tick_params(which='both', direction="in", grid_color='grey', grid_alpha=0.2)
+
+		fig.suptitle(f"Local and Global Consistency Node Prediction Performance for \"{dataset}\" Dataset")
+		fig.tight_layout()
+
+		fig.savefig(f"./figs/q1_lgc/{dataset}.png", format="png")
+		plt.close()
+
+
